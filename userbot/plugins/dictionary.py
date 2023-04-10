8160aa0620c6c993d941513f9b5fd6f3c4b20e69
Karl <58265182+KarlKennerley@users.noreply.github.com>
2023-04-10 11:02:50 +0100

    Update dictionary.py

--- userbot/plugins/dictionary.py
+++ userbot/plugins/dictionary.py
@@ -9,13 +9,15 @@ import requests
 @app.on_message(filters.outgoing & filters.text & filters.command(["udcit", "ud", "urbandictionary"], prefixes=config["prefixes"]))
 async def urbandictionary(_, msg):
     chat = msg.chat
-    term = " ".jooin(parse_args(msg))
+    term = " ".join(parse_args(msg))
     try:
-        data = requests.get(f"https://api.urbandictionary.com/v0/define?term={term}").json()['list'][0]['definition'].replace("[", "").replace("]", "")
-        await msg.edit_text(f"'{term}' is defined as:\n{data}")
-        await msg.react(emoji="👍")
+        data = requests.get(f"https://api.urbandictionary.com/v0/define?term={term}").json()['list'][0]
+        definition = data['definition'].replace("[", "").replace("]", "")
+        example = data['example'].replace("[", "").replace("]", "")
+        thumbs_up = data['thumbs_up']
+        thumbs_down = data['thumbs_down']
+        await msg.edit_text(f"'{term}' is defined as:\n{definition}\nExample usage:\n__{example}__\nDefinition rating: 👍{thumbs_up}/👎{thumbs_down}")
     except:
       await msg.edit_text(f"I'm unable to find a definition for '{term}'.")
-      await msg.react(emoji="👎")
       
     return
