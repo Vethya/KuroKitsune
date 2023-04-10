44bb8603d25d0c447f572a891c5f6234e8e5794e
Karl <58265182+KarlKennerley@users.noreply.github.com>
2023-04-10 13:11:55 +0100

    Moved wikipedia command to new file

--- userbot/plugins/dictionary.py
+++ userbot/plugins/dictionary.py
@@ -22,13 +22,3 @@ async def urbandictionary(_, msg):
       await msg.edit_text(f"I'm unable to find a definition for '{term}'.")
       
     return
-
-@app.on_message(filters.outgoing & filters.text & filters.command(["wikipedia"], prefixes=config["prefixes"]))
-async def wikipedia(_, msg):
-    chat = msg.chat
-    term = " ".join(parse_args(msg))
-    try:
-        page = wikipedia.page(term)
-        msg.edit_text(f"Here is the Wikipedia summary for {term}:\n{page.summary}\nYou can find more information on {page.title} [here]({page.url}) ")
-    except:
-        await msg.edit_text(f"I'm unable to find a wikipedia entry for '{term}'.")
