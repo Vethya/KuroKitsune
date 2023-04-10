7171c2efedd34fa289ba251ea5477e57bb5746f1
Vethya <vethyaprogamer@gmail.com>
2023-04-10 20:47:43 +0700

    Clean Wikipedia Module

--- userbot/plugins/wikipedia.py
+++ userbot/plugins/wikipedia.py
@@ -5,13 +5,13 @@ from pyrogram import filters
 
 import wikipediaapi
 
-@app.on_message(filters.outgoing & filters.text & filters.command(["wikipedia"], prefixes=config["prefixes"]))
+@app.on_message(filters.outgoing & filters.text & filters.command(["wiki", "wikipedia"], prefixes=config["prefixes"]))
 async def wikipedia(_, msg):
-    chat = msg.chat
     term = " ".join(parse_args(msg))
     wiki = wikipediaapi.Wikipedia('en')
+
     try:
         page = wiki.page(term)
-        await msg.edit_text(f"Here is the Wikipedia summary for {term}:\n{page.summary}\nYou can find more information on {page.title} [here]({page.fullurl}).")
+        await msg.edit_text(f"<b>Here is the Wikipedia summary for {term}:</b>\n{page.summary}\n\nYou can find more information on {page.title} [here]({page.fullurl}).")
     except:
         await msg.edit_text(f"I'm unable to find a wikipedia entry for '{term}'.")
